cf4b159fca38944f7de9dbed4eeec2c24918aab6
softflied <102998343+softflied@users.noreply.github.com>
2023-01-10 16:03:50 +0300

    .

---
+++ TicTacToe/Assets/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    AudioManager audioManager;
+    public GameObject persistantObj;
+
+    public string gameMode;
+
+    public void OnClicked()
+    {
+      
+        persistantObj.GetComponent<PersistanceScript>().gameMode = gameMode;
+        SceneManager.LoadScene("GameScreen");
+    }
+}
